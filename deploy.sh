21f3b0852b60bfc4f5ad1b8d959b758103307d1e
yindaheng98 <yindaheng98@163.com>
2019-09-23 11:54:26 +0800

    test build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git config user.name "TravisCI"
 git config user.email "yindaheng98@163.com"
-sed -i "s/DEPLOY_REPO/$1/g" ./_config.yml
+sed -i "s/DEPLOY_TOKEN/$1/g" ./_config.yml
 cat ./_config.yml
 set -e
 hexo deploy
